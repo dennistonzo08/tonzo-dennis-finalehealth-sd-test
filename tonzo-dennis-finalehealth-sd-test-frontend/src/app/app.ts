f41d81317d60ab0c3c820deb9c19aa6d73bf17ce
dennistonzo08 <dennistonzo08@gmail.com>
2025-07-12 07:23:48 +0800

    add navbar and additional information for patient

--- tonzo-dennis-finalehealth-sd-test-frontend/src/app/app.ts
+++ tonzo-dennis-finalehealth-sd-test-frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Patients } from '../patients/patients';
 import { Visits } from '../visits/visits';
+import { Navbar } from '../navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Patients,Visits],
+  imports: [RouterOutlet,Patients,Visits,Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
